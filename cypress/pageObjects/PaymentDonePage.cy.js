08f73952e4e355760461c9cb38086bb5d6f8c19e
Anton Kolupaiev <antnikol@gmail.com>
2024-12-24 01:37:18 +0200

    add POM test #24

--- cypress/pageObjects/PaymentDonePage.cy.js
+++ cypress/pageObjects/PaymentDonePage.cy.js
@@ -6,7 +6,19 @@ class PaymentDonePage extends BasePage {
 
 getOrderPlacedHeading = () => cy.get('h2[data-qa="order-placed"]')
 getOrderPlacedMessage = () => cy.get('h2[data-qa="order-placed"] + p')
+getDownloadInvoiceButton = () => cy.get('.btn.btn-default.check_out')
+getContinuePlacedOrderButton = () => cy.get('a[data-qa="continue-button"]')
 
+
+clickDownloadInvoiceButton() {
+  this.getDownloadInvoiceButton().click()
+  return this
+}
+
+clickContinuePlacedOrderButton() {
+  this.getContinuePlacedOrderButton().click()
+  return this
+}
 
 
 }
