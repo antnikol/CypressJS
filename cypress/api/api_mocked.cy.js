5afcbc1c2764d53ea74be92c473886765ec10369
Anton Kolupaiev <antnikol@gmail.com>
2024-12-25 17:06:47 +0200

    upd mocked test

--- cypress/api/api_mocked.cy.js
+++ cypress/api/api_mocked.cy.js
@@ -3,23 +3,32 @@ import { liveServerUrl, message, baseUrl, gitHubActionsServerUrl } from '../fixt
 
 describe('API tests with mocked data', () => {
   beforeEach(() => {
-    Cypress.config('baseUrl', gitHubActionsServerUrl)
+    Cypress.config('baseUrl', 'http://localhost:3000')
   })
 
   it('API 1(3): __Mocked_DATA__ Get All Products List', () => {
-    cy.intercept('GET', '/api/productsList', {
-      statusCode: 201,
-      body: {
-        products: [{ name: message.mocked }],
-      },
-    }).as('mockProdList')
-
+    cy.intercept('GET', '/api/productsList', (req) => {
+      console.log('Intercepted request:', req); 
+      req.reply({
+        statusCode: 201,
+        body: {
+          products: [{ name: message.mocked }],
+        },
+      });
+    }).as('mockProdList');
+  
     cy.visit('/cypress/fixtures/mockPage.html', { failOnStatusCode: false })
+      .then(() => {
+        console.log('Page loaded'); 
+      });
+  
     cy.wait('@mockProdList').then((interception) => {
+      console.log('Intercepted response:', interception.response); 
       expect(interception.response.statusCode).to.eq(201);
       expect(interception.response.body.products[0].name).to.eq(message.mocked);
-    })
-  })
+    });
+  });
+  
 
   afterEach(() => {
     Cypress.config('baseUrl', baseUrl)
